1065a1ce97bdb71b7180943ef97e08c5323b96fc
Brendon Richards <brendonrichards93@gmail.com>
2018-10-02 08:21:25 +0000

    Include missing Navbar and NavbarBrand

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   render() {
